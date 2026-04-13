ff8254410aa62c03781572172d59d346a810128e
oscarhinojosatk-design <oscarhinojosatk@gmail.com>
2026-04-12 20:10:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,12 @@ if st.button("Generate Growth Assets"):
             prompt = f"""
             You are the Lead Growth Architect for EPIC Lab.
             Translate the following text into three assets:
-            1. A TikTok Script (focus on loss aversion/FOMO for the hook, you can use )
-            2. 3 Google Ads/meta ads variations (Character optimized for CTR, you should implemment behavioral ecnom)
+            1. A TikTok Script (focus on loss aversion/FOMO for the hook, you can use several types of tiktok to engage audience, telling a story, 
+            acting, if you make a sketch, you can use a humor that appelas to entrepeneurs, people into business, etc. use characters like a finance bro, crypto bro
+            people qhi want to get outside the matrix, 9-5 rejection, buy be sure that the final message is that the epic lab is the institutional right way and professional way to do it, this sketch thing 
+            only if it fits, you can use other type of videos, )
+            2. 3 Google Ads/meta ads variations (Character optimized for CTR, you should implemment behavioral economics biases, loss aversion, anchoring etc...
+            for the output please create  1. key words for the add. 2. titles 3. descriptions 4. sugggested images)
             3. A Landing Page Wireframe (Hero, Social Proof, 3 Pillars)
             
             Text to translate:
